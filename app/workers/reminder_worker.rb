cacba2641ca503fdfabdcbfc6ad8ab1469478819
olaide ojewale <olaide.ojewale@andela.com>
2016-06-25 11:59:49 +0100

    Modify worker

--- app/workers/reminder_worker.rb
+++ app/workers/reminder_worker.rb
@@ -1,9 +1,10 @@
 class ReminderWorker
   include Sidekiq::Worker
-  sidekiq_options retry: 5
+  sidekiq_options retry: 5, queue: "default"
   def perform(details)
     user_id = details[:user_id]
     message = "Holla! You said I should remind you that: #{details[:user]}"
+    puts "getting here!"
     VerifyController.new.send(:make_request, user_id, message)
   end
 end
