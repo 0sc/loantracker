9c59528d3f01f57381a68da5f745e3b42c59db39
Osmond Oscar <osmond.oranagwa@andela.com>
2016-07-05 00:53:14 +0100

    debug worker

--- app/workers/reminder_worker.rb
+++ app/workers/reminder_worker.rb
@@ -5,6 +5,7 @@ class ReminderWorker
     user_id = details[:user_id]
     message = "Holla! You said I should remind you that: #{details[:user]}"
     puts "getting here!"
+    p details
     VerifyController.new.send(:make_request, user_id, message)
   end
 end
