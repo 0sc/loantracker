352792168c0888510dfb941b06b2100fed55a714
olaide ojewale <olaide.ojewale@andela.com>
2016-07-07 00:16:39 +0100

    Add options for reminders to be set up to a year

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -123,17 +123,29 @@ class VerifyController < ApplicationController
 
   def reminder(msg)
     matched_grps = msg.scan(reminder_pattern).flatten
-    num_hrs = matched_grps[0]
+    time_fig = matched_grps[0].to_i
     details = { user_id: @user.id,
                 fb_user_id: @fb_user_id,
                 message: matched_grps[2]
               }
-    ReminderWorker.perform_in(num_hrs.to_i.minutes, details)
-    "Your reminder has been set. Yippe!"
+    if span = time_span(time_fig, matched_grps[1])
+      ReminderWorker.perform_in(time_fig.send(span), details)
+      "Your reminder has been set. Yippe!"
+    else
+      "Your reminder can not be more than a year."
+    end
   end
 
   def reminder_pattern
-    /remind me in\s(\d+)\s?(mins|minutes)\sthat\s((\w+)\sborrowed\s(\d+))/
+    /remind me in\s(\d+)\s?(min|minute|hr|hour|day|wk|week|month|yr|year)s?\sthat\s((\w+)\sborrowed\s(\d+))/
   end
 
+  def time_span(fig, span)
+    return "years" if (fig == 1) && (span =~ /yr|year/)
+    return "months" if (fig < 13) && (span =~ /month/)
+    return "weeks" if (fig < 53) && (span =~ /wk|week/)
+    return "days" if (fig < 367) && (span =~ /day/)
+    return "hours" if (fig < 8785) && (span =~ /hr|hour/)
+    "minutes" if (fig < 527041) && (span =~ /min|minute/)
+  end
 end
