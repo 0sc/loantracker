56562c0f91f56aae150807e76c0d1463eab7b860
olaide ojewale <olaide.ojewale@andela.com>
2016-07-07 12:01:25 +0100

    Correct class method vs instance method bug in Reminder

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -13,25 +13,23 @@ class Reminder
     def reminder_pattern
       /remind me in\s(\d+)\s?(min|minute|hr|hour|day|wk|week|month|yr|year)s?\sthat\s((\w+)\sborrowed\s(\d+))/
     end
-  end
 
-  private
-
-  def set_reminder(fig, category, details)
-    if span = time_span(fig, category)
-      ReminderWorker.perform_in(fig.send(span), details)
-      "Your reminder has been set. Yippe!"
-    else
-      "Your reminder can not be more than a year."
+    def set_reminder(fig, category, details)
+      if span = time_span(fig, category)
+        ReminderWorker.perform_in(fig.send(span), details)
+        "Your reminder has been set. Yippe!"
+      else
+        "Your reminder can not be more than a year."
+      end
     end
-  end
 
-  def time_span(fig, span)
-    return "years" if (fig == 1) && (span =~ /yr|year/)
-    return "months" if (fig < 13) && (span =~ /month/)
-    return "weeks" if (fig < 53) && (span =~ /wk|week/)
-    return "days" if (fig < 367) && (span =~ /day/)
-    return "hours" if (fig < 8785) && (span =~ /hr|hour/)
-    "minutes" if (fig < 527041) && (span =~ /min|minute/)
+    def time_span(fig, span)
+      return "years" if (fig == 1) && (span =~ /yr|year/)
+      return "months" if (fig < 13) && (span =~ /month/)
+      return "weeks" if (fig < 53) && (span =~ /wk|week/)
+      return "days" if (fig < 367) && (span =~ /day/)
+      return "hours" if (fig < 8785) && (span =~ /hr|hour/)
+      "minutes" if (fig < 527041) && (span =~ /min|minute/)
+    end
   end
 end
