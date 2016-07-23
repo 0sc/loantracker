905cac18c94b1645c557352ce6f03ca801c9d39f
Osmond Oscar <osmond.oranagwa@andela.com>
2016-07-23 16:09:37 +0100

    move transaction record keeping to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,13 @@
 class Transaction < ActiveRecord::Base
   belongs_to :user
+
+  def self.save_transaction(user, action, amount, debtor)
+    create({
+        description: "#{debtor.name} #{action} #{amount.to_f}",
+        debtor: debtor.name,
+        amount: amount.to_f,
+        balance: debtor.amount,
+        user: user
+      })
+  end
 end
