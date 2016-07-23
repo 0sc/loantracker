4e8920d768736cddee27508b2bd3b98f906b0ae3
Osmond Oscar <osmond.oranagwa@andela.com>
2016-07-23 16:09:01 +0100

    add after_destroy model hook to save delete debtor transactions

--- app/models/debtor.rb
+++ app/models/debtor.rb
@@ -1,3 +1,12 @@
 class Debtor < ActiveRecord::Base
   belongs_to :user
+
+  after_destroy :record_transaction
+
+  def record_transaction
+    user.transactions.create({
+        description: "You removed #{name} from your debtors list",
+        debtor: name
+      })
+  end
 end
